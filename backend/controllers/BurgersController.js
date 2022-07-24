35e6ed36aa3a5f641ece3b7b3577786c12a6bc3a
AizeN1337 <maciejkalata79@gmail.com>
2022-07-24 22:38:14 +0200

    Sidebar redesign

--- backend/controllers/BurgersController.js
+++ backend/controllers/BurgersController.js
@@ -1,10 +1,12 @@
 const { getDocs, collection} = require('firebase/firestore')
 const { database  } = require('../firebase/firebaseConfig')
 const getAllBurgers = async (req,res) => {
-    const snapshot = await getDocs(collection(database,"burgers"))
-    snapshot.forEach((doc) => {
-        console.log(JSON.stringify({id: doc.id, ...doc.data()}))
-    });
-    res.send({message: "Successfully send"})
+    try {
+        const snapshot = await getDocs(collection(database,"burgers"))
+        res.status(200).json(snapshot)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
 }
 module.exports = getAllBurgers 
